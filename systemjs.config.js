0786b56233ca2810aedc1a481e94d6bbe5221eb1
bruehldev <bruehl.dev@gmail.com>
2016-06-26 13:31:36 +0200

    File structure implementet

--- systemjs.config.js
+++ systemjs.config.js
@@ -5,16 +5,16 @@
 (function(global) {
   // map tells the System loader where to look for things
   var map = {
-    'app':                        'app', // 'dist',
+    'app':                        'src', // 'dist',
     '@angular':                   'node_modules/@angular',
     'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
     'rxjs':                       'node_modules/rxjs'
   };
   // packages tells the System loader how to load when no filename and/or no extension
   var packages = {
-    'app':                        { main: 'main.js',  defaultExtension: 'js' },
+    'app':                        { main: 'app/main.js',  defaultExtension: 'js' },
     'rxjs':                       { defaultExtension: 'js' },
-    'angular2-in-memory-web-api': { main: 'index.js', defaultExtension: 'js' },
+    'angular2-in-memory-web-api': { main: 'app/index.js', defaultExtension: 'js' },
   };
   var ngPackageNames = [
     'common',
