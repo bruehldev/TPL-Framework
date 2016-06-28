ee77ea9e473908383e69a8aeb6aade515807c977
bruehldev <bruehl.dev@gmail.com>
2016-06-28 12:28:10 +0200

    Added navigation and sidebar example to fill with polymer elements

---
+++ src/theme/data-view/data-view.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'main_view',
+  templateUrl: 'src/theme/data-view/data-view.component.html',
+  styles: []
+})
+export class Navbar{
+}
